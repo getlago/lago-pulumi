0ce12bfba54be6c9e6f395a2fa5628668ba537ac
tomfrew <tom@keel.xyz>
2024-02-09 21:41:24 +0000

    Base setup

--- __main__.py
+++ __main__.py
@@ -11,81 +11,100 @@ import database
 import cluster
 import frontend
 import backend
+import bucket
 
 config = pulumi.Config()
-service_name = config.get('service_name') or 'lago'
-lago_version = config.get('lago_version')
-db_name = config.get('db_name') or 'lago'
-db_user = config.get('db_user') or 'lago'
+service_name = config.get("service_name") or "lago"
+lago_version = config.get("lago_version")
+db_name = config.get("db_name") or "lago"
+db_user = config.get("db_user") or "lago"
 
-db_password = config.get_secret('db_password')
+db_password = config.get_secret("db_password")
 if not db_password:
-  password = random.RandomPassword('db_password',
-    length=16,
-    special=True,
-    override_special='_%',
-  )
-  db_password = password.result
+    password = random.RandomPassword(
+        "db_password",
+        length=16,
+    )
+    db_password = password.result
 
 # Create an AWS VPC with Subnets and Security Groups
-network = network.Vpc(f'{service_name}-net', network.VpcArgs())
+network = network.Vpc(f"{service_name}-net", network.VpcArgs())
 subnet_ids = []
 for subnet in network.subnets:
-  subnet_ids.append(subnet.id)
+    subnet_ids.append(subnet.id)
 
 # Create an RDS PostgreSQL instance
-db = database.Db(f'{service_name}-db',
-  database.DbArgs(
-    db_name=db_name,
-    db_user=db_user,
-    db_password=db_password,
-    subnet_ids=subnet_ids,
-    security_group_ids=[network.rds_security_group.id],
-  ),
+db = database.Db(
+    f"{service_name}-db",
+    database.DbArgs(
+        db_name=db_name,
+        db_user=db_user,
+        db_password=db_password,
+        subnet_ids=subnet_ids,
+        security_group_ids=[network.rds_security_group.id],
+    ),
 )
 
 # Create a Elasticache Redis instance
-redis = database.Redis(f'{service_name}-redis',
-  database.RedisArgs(
-    redis_name=db_name,
-    subnet_ids=subnet_ids,
-    security_group_ids=[network.redis_security_group.id],
-  ),
+redis = database.Redis(
+    f"{service_name}-redis",
+    database.RedisArgs(
+        redis_name=db_name,
+        subnet_ids=subnet_ids,
+        security_group_ids=[network.redis_security_group.id],
+    ),
 )
 
 # Create ECS Cluster
-cluster = cluster.Cluster(f'{service_name}-ecs')
+cluster = cluster.Cluster(f"{service_name}-ecs")
+
+bucket = bucket.Bucket(
+    f"keel-{service_name}-storage", bucket.BucketArgs(role_name=cluster.role.name)
+)
 
 # Create Backend
-backend = backend.Backend(f'{service_name}-be', backend.BackendArgs(
-  cluster_arn=cluster.cluster.arn,
-  role_arn=cluster.role.arn,
-  lago_version=lago_version,
-  vpc_id=network.vpc.id,
-  subnet_ids=subnet_ids,
-  security_group_ids=[network.app_security_group.id],
-  db_host=db.db.address,
-  db_name=db_name,
-  db_user=db_user,
-  db_password=db_password,
-  redis_host=redis.redis.cache_nodes[0].address,
-))
+backend = backend.Backend(
+    f"{service_name}-be",
+    backend.BackendArgs(
+        cluster_arn=cluster.cluster.arn,
+        role=cluster.role,
+        lago_version=lago_version,
+        vpc_id=network.vpc.id,
+        subnet_ids=subnet_ids,
+        app_security_group=network.app_security_group,
+        container_security_group=network.be_security_group,
+        db_host=db.db.address,
+        db_name=db_name,
+        db_user=db_user,
+        db_password=db_password,
+        redis_host=redis.redis.cache_nodes[0].address,
+        bucket_name=bucket.bucket.bucket,
+        front_url=network.front_lb.dns_name,
+        api_url=network.back_lb.dns_name,
+        alb_arn=network.back_lb.arn,
+    ),
+)
 
 # Create Frontend
-front = frontend.Frontend(f'{service_name}-front', frontend.FrontendArgs(
-  cluster_arn=cluster.cluster.arn,
-  role_arn=cluster.role.arn,
-  lago_version=lago_version,
-  vpc_id=network.vpc.id,
-  subnet_ids=subnet_ids,
-  security_group_ids=[network.app_security_group.id],
-))
+front = frontend.Frontend(
+    f"{service_name}-front",
+    frontend.FrontendArgs(
+        cluster_arn=cluster.cluster.arn,
+        role=cluster.role,
+        lago_version=lago_version,
+        vpc_id=network.vpc.id,
+        subnet_ids=subnet_ids,
+        security_group_ids=[network.app_security_group.id],
+        api_url=network.back_lb.dns_name,
+        alb_arn=network.front_lb.arn,
+    ),
+)
 
-front_url = pulumi.Output.concat('http://', front.alb.dns_name)
-api_url = pulumi.Output.concat('http://', backend.alb.dns_name)
+front_url = pulumi.Output.concat("http://", network.front_lb.dns_name)
+api_url = pulumi.Output.concat("http://", network.back_lb.dns_name)
 
-pulumi.export('Lago Front URL', front_url)
-pulumi.export('Lago API URL', api_url)
-pulumi.export('ECS Cluster Name', cluster.cluster.name)
-pulumi.export('Lago Version', lago_version)
-pulumi.export('Service Name', service_name)
+pulumi.export("Lago Front URL", front_url)
+pulumi.export("Lago API URL", api_url)
+pulumi.export("ECS Cluster Name", cluster.cluster.name)
+pulumi.export("Lago Version", lago_version)
+pulumi.export("Service Name", service_name)
--- backend.py
+++ backend.py
@@ -1,245 +1,310 @@
 import base64
 import json
-import pulumi
 import pulumi_random as random
 import pulumi_tls as tls
 
 from pulumi import ComponentResource, Output, ResourceOptions
-from pulumi_aws import lb, ecs
+from pulumi_aws import lb, ecs, cloudwatch, iam, config, get_caller_identity
+
 
 class BackendArgs:
-  
-  def __init__(self,
-               lago_version=None,
-               cluster_arn=None,
-               role_arn=None,
-               vpc_id=None,
-               subnet_ids=None,
-               security_group_ids=None,
-               db_host=None,
-               db_port='5432',
-               db_name=None,
-               db_user=None,
-               db_password=None,
-               redis_host=None,
-               redis_port='6379',
-              ):
-    self.lago_version = lago_version
-    self.cluster_arn = cluster_arn
-    self.role_arn = role_arn
-    self.vpc_id = vpc_id
-    self.subnet_ids = subnet_ids
-    self.security_group_ids = security_group_ids
-    self.db_host = db_host
-    self.db_port = db_port
-    self.db_name = db_name
-    self.db_user = db_user
-    self.db_password = db_password
-    self.redis_host = redis_host
-    self.redis_port = redis_port
+
+    def __init__(
+        self,
+        lago_version=None,
+        cluster_arn=None,
+        role={},
+        vpc_id=None,
+        subnet_ids=None,
+        app_security_group=None,
+        container_security_group=None,
+        db_host=None,
+        db_port="5432",
+        db_name=None,
+        db_user=None,
+        db_password=None,
+        redis_host=None,
+        redis_port="6379",
+        bucket_name=None,
+        front_url=None,
+        alb_arn=None,
+        api_url=None,
+    ):
+        self.lago_version = lago_version
+        self.cluster_arn = cluster_arn
+        self.role = role
+        self.vpc_id = vpc_id
+        self.subnet_ids = subnet_ids
+        self.app_security_group_id = app_security_group
+        self.container_security_group = container_security_group
+        self.db_host = db_host
+        self.db_port = db_port
+        self.db_name = db_name
+        self.db_user = db_user
+        self.db_password = db_password
+        self.redis_host = redis_host
+        self.redis_port = redis_port
+        self.bucket_name = bucket_name
+        self.front_url = front_url
+        self.alb_arn = alb_arn
+        self.api_url = api_url
+
 
 class Backend(ComponentResource):
 
-  def __init__(self,
-               name: str,
-               args: BackendArgs,
-               opts: ResourceOptions = None,
-              ):
-    super().__init__('custom:resource:Backend', name, {}, opts)
+    def __init__(
+        self,
+        name: str,
+        args: BackendArgs,
+        opts: ResourceOptions = None,
+    ):
+        super().__init__("custom:resource:Backend", name, {}, opts)
 
-    # Create a Load Balancer to listen to HTTP traffic on port 80
-    self.alb = lb.LoadBalancer(f'{name}-alb',
-      security_groups=args.security_group_ids,
-      subnets=args.subnet_ids,
-      opts=ResourceOptions(parent=self),
-    )
+        # Create a Load Balancer to listen to HTTP traffic on port 80
+        self.alb = lb.LoadBalancer(
+            f"{name}-alb",
+            security_groups=[args.app_security_group_id],
+            subnets=args.subnet_ids,
+            opts=ResourceOptions(parent=self),
+        )
 
-    # Create a Target Group for API
-    api_target_group = lb.TargetGroup(f'{name}-api-tg',
-      port=3000,
-      protocol='HTTP',
-      target_type='ip',
-      vpc_id=args.vpc_id,
-      health_check=lb.TargetGroupHealthCheckArgs(
-        path='/health',
-        port=3000,
-        healthy_threshold=2,
-        interval=5,
-        timeout=4,
-        protocol='HTTP',
-        matcher='200-399',
-      ),
-      opts=ResourceOptions(parent=self),  
-    )
+        # Create a Target Group for API
+        api_target_group = lb.TargetGroup(
+            f"{name}-api-tg",
+            port=3000,
+            protocol="HTTP",
+            target_type="ip",
+            vpc_id=args.vpc_id,
+            health_check=lb.TargetGroupHealthCheckArgs(
+                path="/health",
+                port=3000,
+                healthy_threshold=2,
+                interval=5,
+                timeout=4,
+                protocol="HTTP",
+                matcher="200-399",
+            ),
+            opts=ResourceOptions(parent=self),
+        )
 
-    # Create a listener for API
-    api_listener = lb.Listener(f'{name}-api-listener',
-      load_balancer_arn=self.alb.arn,
-      port=80,
-      default_actions=[lb.ListenerDefaultActionArgs(
-        type='forward',
-        target_group_arn=api_target_group.arn,
-      )],
-      opts=ResourceOptions(parent=self),
-    )
-    
-    database_url = Output.concat(
-      'postgres://',
-      args.db_user,
-      ':',
-      args.db_password,
-      '@',
-      args.db_host,
-      ':',
-      args.db_port,
-      '/',
-      args.db_name,
-    )
+        # Create a listener for API
+        api_listener = lb.Listener(
+            f"{name}-api-listener",
+            load_balancer_arn=args.alb_arn,
+            port=80,
+            default_actions=[
+                lb.ListenerDefaultActionArgs(
+                    type="forward",
+                    target_group_arn=api_target_group.arn,
+                )
+            ],
+            opts=ResourceOptions(parent=self),
+        )
 
-    redis_url = Output.concat(
-      'redis://',
-      args.redis_host,
-      ':',
-      args.redis_port,
-    )
+        database_url = Output.concat(
+            "postgres://",
+            args.db_user,
+            ":",
+            args.db_password,
+            "@",
+            args.db_host,
+            ":",
+            args.db_port,
+            "/",
+            args.db_name,
+        )
 
-    rsa_private_key = tls.PrivateKey(f'{name}-private-key',
-      algorithm='RSA',
-    ).private_key_pem.apply(lambda key: base64.b64encode(key.encode()).decode())
-    secret_key_base = random.RandomString(f'{name}-secret-key-base',
-      length=64,
-      special=False,
-    ).result.apply(lambda key: base64.b64encode(key.encode()).decode())
-    encryption_deterministic_key = random.RandomString(f'{name}-encryption-deterministic-key',
-      length=32,
-      special=False,
-    ).result.apply(lambda key: base64.b64encode(key.encode()).decode())
-    encryption_key_derivation_salt = random.RandomString(f'{name}-encryption-key-derivation-salt',
-      length=32,
-      special=False,
-    ).result.apply(lambda key: base64.b64encode(key.encode()).decode())
-    encryption_primary_key = random.RandomString(f'{name}-encryption-primary-key',
-      length=32,
-      special=False,
-    ).result.apply(lambda key: base64.b64encode(key.encode()).decode())
+        redis_url = Output.concat(
+            "redis://",
+            args.redis_host,
+            ":",
+            args.redis_port,
+        )
 
-    # Create the API ECS Task Definition
-    api_task_name = f'{name}-api-task'
-    api_container_name = f'{name}-api-container'
-    self.api_task_definition = ecs.TaskDefinition(api_task_name,
-      family=api_task_name,
-      cpu='1024',
-      memory='2048',
-      network_mode='awsvpc',
-      requires_compatibilities=['FARGATE'],
-      execution_role_arn=args.role_arn,
-      container_definitions=Output.json_dumps([{
-        'name': api_container_name,
-        'image': f'getlago/api:v{args.lago_version}',
-        'portMappings': [{
-          'containerPort': 3000,
-          'hostPort': 3000,
-          'protocol': 'tcp',
-        }],
-        'environment': [
-          {
-            'name': 'RAILS_ENV',
-            'value': 'production',
-          },
-          {
-            'name': 'DATABASE_URL',
-            'value': database_url,
-          },
-          {
-            'name': 'REDIS_URL',
-            'value': redis_url,
-          },
-          {
-            'name': 'REDIS_CACHE_URL',
-            'value': redis_url,
-          },
-          {
-            'name': 'LAGO_SIDEKIQ_WEB',
-            'value': 'true',
-          },
-          {
-            'name': 'RAILS_LOG_TO_STDOUT',
-            'value': 'true',
-          },
-          {
-            'name': 'LAGO_RSA_PRIVATE_KEY',
-            'value': rsa_private_key,
-          },
-          {
-            'name': 'SECRET_KEY_BASE',
-            'value': secret_key_base,
-          },
-          {
-            'name': 'ENCRYPTION_DETERMINISTIC_KEY',
-            'value': encryption_deterministic_key,
-          },
-          {
-            'name': 'ENCRYPTION_KEY_DERIVATION_SALT',
-            'value': encryption_key_derivation_salt,
-          },
-          {
-            'name': 'ENCRYPTION_PRIMARY_KEY',
-            'value': encryption_primary_key,
-          },
-          {
-            'name': 'LAGO_DISABLE_SEGMENT',
-            'value': 'true',
-          },
-          {
-            'name': 'LAGO_DISABLE_SIGNUP',
-            'value': 'false',
-          },
-          {
-            'name': 'DATABASE_POOL',
-            'value': '10',
-          },
-          {
-            'name': 'RAILS_MAX_THREADS',
-            'value': '5',
-          },
-          {
-            'name': 'RAILS_MIN_THREADS',
-            'value': '0',
-          },
-          {
-            'name': 'SIDEKIQ_EVENTS',
-            'value': 'true',
-          },
-          {
-            'name': 'WEB_CONCURRENCY',
-            'value': '2',
-          },
-          {
-            'name': 'LAGO_USE_AWS_S3',
-            'value': 'true',
-          },
-        ],
-      }]),
-      opts=ResourceOptions(parent=self),
-    )
+        rsa_private_key = tls.PrivateKey(
+            f"{name}-private-key",
+            algorithm="RSA",
+        ).private_key_pem.apply(lambda key: base64.b64encode(key.encode()).decode())
+        secret_key_base = random.RandomString(
+            f"{name}-secret-key-base",
+            length=64,
+            special=False,
+        ).result.apply(lambda key: base64.b64encode(key.encode()).decode())
+        encryption_deterministic_key = random.RandomString(
+            f"{name}-encryption-deterministic-key",
+            length=32,
+            special=False,
+        ).result.apply(lambda key: base64.b64encode(key.encode()).decode())
+        encryption_key_derivation_salt = random.RandomString(
+            f"{name}-encryption-key-derivation-salt",
+            length=32,
+            special=False,
+        ).result.apply(lambda key: base64.b64encode(key.encode()).decode())
+        encryption_primary_key = random.RandomString(
+            f"{name}-encryption-primary-key",
+            length=32,
+            special=False,
+        ).result.apply(lambda key: base64.b64encode(key.encode()).decode())
 
-    # Create the API ECS Service
-    self.api_service = ecs.Service(f'{name}-api-svc',
-      cluster=args.cluster_arn,
-      desired_count=1,
-      launch_type='FARGATE',
-      task_definition=self.api_task_definition.arn,
-      network_configuration=ecs.ServiceNetworkConfigurationArgs(
-        assign_public_ip=True,
-        subnets=args.subnet_ids,
-        security_groups=args.security_group_ids,
-      ),
-      load_balancers=[ecs.ServiceLoadBalancerArgs(
-        target_group_arn=api_target_group.arn,
-        container_name=api_container_name,
-        container_port=3000,
-      )],
-      opts=ResourceOptions(depends_on=[api_listener], parent=self),
-    )
+        environment = [
+            {
+                "name": "RAILS_ENV",
+                "value": "production",
+            },
+            {
+                "name": "DATABASE_URL",
+                "value": database_url,
+            },
+            {
+                "name": "REDIS_URL",
+                "value": redis_url,
+            },
+            {
+                "name": "REDIS_CACHE_URL",
+                "value": redis_url,
+            },
+            {
+                "name": "LAGO_SIDEKIQ_WEB",
+                "value": "true",
+            },
+            {
+                "name": "RAILS_LOG_TO_STDOUT",
+                "value": "true",
+            },
+            {
+                "name": "LAGO_RSA_PRIVATE_KEY",
+                "value": rsa_private_key,
+            },
+            {
+                "name": "SECRET_KEY_BASE",
+                "value": secret_key_base,
+            },
+            {
+                "name": "ENCRYPTION_DETERMINISTIC_KEY",
+                "value": encryption_deterministic_key,
+            },
+            {
+                "name": "ENCRYPTION_KEY_DERIVATION_SALT",
+                "value": encryption_key_derivation_salt,
+            },
+            {
+                "name": "ENCRYPTION_PRIMARY_KEY",
+                "value": encryption_primary_key,
+            },
+            {
+                "name": "LAGO_DISABLE_SEGMENT",
+                "value": "true",
+            },
+            {
+                "name": "LAGO_DISABLE_SIGNUP",
+                "value": "false",
+            },
+            {
+                "name": "DATABASE_POOL",
+                "value": "10",
+            },
+            {
+                "name": "RAILS_MAX_THREADS",
+                "value": "5",
+            },
+            {
+                "name": "RAILS_MIN_THREADS",
+                "value": "0",
+            },
+            {
+                "name": "SIDEKIQ_EVENTS",
+                "value": "true",
+            },
+            {
+                "name": "WEB_CONCURRENCY",
+                "value": "2",
+            },
+            {
+                "name": "LAGO_USE_AWS_S3",
+                "value": "true",
+            },
+            {
+                "name": "AWS_REGION",
+                "value": "eu-west-2",
+            },
+            {
+                "name": "LAGO_AWS_S3_REGION",
+                "value": "eu-west-2",
+            },
+            {
+                "name": "LAGO_AWS_S3_BUCKET",
+                "value": args.bucket_name,
+            },
+            {
+                "name": "LAGO_FRONT_URL",
+                "value": Output.concat("http://", args.front_url),
+            },
+            {
+                "name": "LAGO_API_URL",
+                "value": Output.concat("http://", args.api_url),
+            },
+        ]
 
-    self.register_outputs({})
+        # Create the API ECS Task Definition
+        api_task_name = f"{name}-api-task"
+        api_container_name = f"{name}-api-container"
+        self.api_task_definition = ecs.TaskDefinition(
+            api_task_name,
+            family=api_task_name,
+            cpu="1024",
+            memory="2048",
+            network_mode="awsvpc",
+            requires_compatibilities=["FARGATE"],
+            execution_role_arn=args.role.arn,
+            task_role_arn=args.role.arn,
+            container_definitions=Output.json_dumps(
+                [
+                    {
+                        "name": api_container_name,
+                        "image": f"getlago/api:v{args.lago_version}",
+                        "portMappings": [
+                            {
+                                "containerPort": 3000,
+                                "hostPort": 3000,
+                                "protocol": "tcp",
+                            }
+                        ],
+                        "logConfiguration": {
+                            "logDriver": "awslogs",
+                            "options": {
+                                "awslogs-create-group": "true",
+                                "awslogs-group": f"/ecs/{name}-task",
+                                "awslogs-region": "eu-west-2",
+                                "awslogs-stream-prefix": "ecs",
+                            },
+                        },
+                        "environment": environment,
+                    }
+                ]
+            ),
+            opts=ResourceOptions(parent=self),
+        )
+
+        # Create the API ECS Service
+        self.api_service = ecs.Service(
+            f"{name}-api-svc",
+            cluster=args.cluster_arn,
+            desired_count=1,
+            launch_type="FARGATE",
+            task_definition=self.api_task_definition.arn,
+            network_configuration=ecs.ServiceNetworkConfigurationArgs(
+                assign_public_ip=True,
+                subnets=args.subnet_ids,
+                security_groups=[args.container_security_group],
+            ),
+            load_balancers=[
+                ecs.ServiceLoadBalancerArgs(
+                    target_group_arn=api_target_group.arn,
+                    container_name=api_container_name,
+                    container_port=3000,
+                )
+            ],
+            opts=ResourceOptions(depends_on=[api_listener], parent=self),
+        )
+
+        self.register_outputs({})
